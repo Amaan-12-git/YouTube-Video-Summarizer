12438966a162ac77548e864c566db18ad9447644
Mohd Amaan Ansari <muhammadamaan0034@gmail.com>
2025-08-04 00:17:32 +0530

    Update files

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { IoSend } from "react-icons/io5";
+import { MdDelete } from "react-icons/md";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Dashboard = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  useEffect(() => {
+    if (modalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [modalOpen]);
   return (
     <>
-      <Navbar />
-      <div className="w-screen h-[91vh]">
-        <div className="bg-[#121212] w-full h-full flex justify-center items-center">
-          <div className="lg:w-[60%] sm:w-[80%] w-[90%] h-full flex flex-col justify-between items-center">
+      <div className={`${modalOpen ? "blur-sm" : ""} transition duration-300`}>
+        <Navbar />
+      </div>
+      <div className="w-screen h-[91vh] ">
+        <div className="bg-[#121212] w-full h-full flex justify-center">
+          <div className="lg:w-[60%] sm:w-[80%] w-[83%] h-full flex flex-col justify-between items-center">
             <div className="w-full flex-1 p-4 overflow-y-auto flex flex-col gap-5">
               <div className="w-full flex justify-end">
                 <div className="user rounded-3xl bg-[#2f2f2f] max-w-[65%] h-fit p-4">
@@ -37,13 +54,42 @@ const Dashboard = () => {
                   id="text"
                   className="w-[94%] h-full bg-[#252525] text-lg p-3 rounded-3xl focus:outline-none placeholder:text-gray-400 text-white resize-none"
                 ></textarea>
-                <button className="mb-3 text-white">
-                  <IoSend size={20} />
+                <button className="mb-3 text-white cursor-pointer">
+                  <IoSend size={20} className="" />
                 </button>
               </div>
             </div>
           </div>
+          <div
+            onClick={() => setModalOpen(true)}
+            className="group fixed top-20 lg:right-28 md:right-10 sm:right-2 right-0 bg-[#2f2f2f] p-3 rounded-full cursor-pointer"
+          >
+            <MdDelete className="text-white md:size-7 size-5" />
+          </div>
         </div>
+        {modalOpen && (
+          <div className="fixed inset-0 bg-black/40 backdrop-blur-sm flex items-center justify-center z-40">
+            <div className="bg-[#2f2f2f] text-white rounded-xl w-[80%] sm:w-[60%] md:w-[30%] p-6">
+              <div className="mb-4 text-center text-lg">Delete all chats?</div>
+              <div className="flex justify-around">
+                <button
+                  onClick={() => setModalOpen(false)}
+                  className="py-2 px-4 bg-gray-200 text-gray-900 rounded-xl hover:bg-gray-300 transition"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={() => {
+                    setModalOpen(false);
+                  }}
+                  className="py-2 px-4 bg-red-500 text-white hover:bg-red-600 rounded-xl"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
